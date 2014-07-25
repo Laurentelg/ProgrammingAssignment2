3f7a04bf5fadc2bf5c5dc2b256ee72e44e84abdc
unknown <laurentelg@gmail.com>
2014-07-25 11:10:34 +0200

    replace m by xt added comments

--- cachematrix.R
+++ cachematrix.R
@@ -4,17 +4,27 @@
 ## This function will cmpute the Matrix Inverve
 ## If the Matrix is already in cahe it will not compute the inverse again but 
 ## use the cahe instead
+## The trick is to use the operator <<- to set a variable in the the global env to be reused accross functions
+## Testing if the object is already around means the invert has already been computed thus using the cache
+## To run this assignment your first need to "Instantiate the object by calling makeCacheMatrix and then call CacheSolve
 
 makeCacheMatrix <- function(x = matrix()) {
 
-  m <- NULL
+  xt <- NULL
+  ## The set function sets the private variables matrix x and invert matrix xt
+  ## x: Origninal matrix
+  ## xt: Invert Matrix
   set <- function(y) {
     x <<- y
-    m <<- NULL
+    xt <<- NULL
   }
-  get <- function() x
-  setSolve <- function(solve) m <<- solve
-  getSolve <- function() m
+  ## Just return the original matrix
+  get <- function() x 
+  setSolve <- function(solve) {
+    xt <<- solve
+  }
+  getSolve <- function() xt
+  ## return a list of function to act on the "class Member"
   list(set = set, get = get,
        setSolve = setSolve,
        getSolve = getSolve)
@@ -25,13 +35,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
-    m <- x$getSolve()
-    if(!is.null(m)) {
+    xt <- x$getSolve()
+    if(!is.null(xt)) {
       message("getting cached data")
-      return(m)
+      return(xt)
     }
     data <- x$get()
-    m <- solve(data, ...)
-    x$setSolve(m)
-    m
+    xt <- solve(data, ...)
+    x$setSolve(xt)
+    xt
 }
