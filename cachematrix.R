b8a1d4fe209fe9f921a3092f47af36aa8582652a
unknown <laurentelg@gmail.com>
2014-07-25 11:39:53 +0200

    Final version

--- cachematrix.R
+++ cachematrix.R
@@ -7,6 +7,7 @@
 ## The trick is to use the operator <<- to set a variable in the the global env to be reused accross functions
 ## Testing if the object is already around means the invert has already been computed thus using the cache
 ## To run this assignment your first need to "Instantiate the object by calling makeCacheMatrix and then call CacheSolve
+## on the result
 
 makeCacheMatrix <- function(x = matrix()) {
 
@@ -41,7 +42,7 @@ cacheSolve <- function(x, ...) {
       return(xt)
     }
     data <- x$get()
-    xt <- solve(data, ...)
+    xt <- solve(data,...)
     x$setSolve(xt)
     xt
 }
